4d88e16bb39da9f06482f021dae9eda1e5e1c61c
KarenJunco <karenbetianajunco@hotmail.com>
2020-11-06 12:11:05 -0300

    se volvio a modificar app.js

--- app.js
+++ app.js
@@ -15,4 +15,9 @@ no changes added to commit (use "git add" and/or "git commit -a") */
 
 //git status
 
+/* On branch master
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        modified:   app.js */
+
 //git commit -m "mensaje del commit" (esto es para etiquetar y enviar a la caja)
