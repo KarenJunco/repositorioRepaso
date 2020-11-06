9d523a4f2a6b671918d44ef2b0a4fe06d890f647
KarenJunco <karenbetianajunco@hotmail.com>
2020-11-06 12:09:07 -0300

    modificaciones en app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+//git init (para inicializar un repositorio local)
+
+// crear o modificar archivos dentro de la carpeta donde se inicializo el repositorio
+
+//git status 
+/* On branch master
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   app.js
+
+no changes added to commit (use "git add" and/or "git commit -a") */
+
+//git add . (para agregar TODO a la caja)
+
+//git status
+
+//git commit -m "mensaje del commit" (esto es para etiquetar y enviar a la caja)
